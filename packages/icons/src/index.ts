2514e072de465c7453e2a09a6aada7334a382a95
Clément Creusat <clement.creusat@opendigitaleducation.com>
2022-08-08 18:45:44 +0200

    monorepo

---
+++ packages/icons/src/index.ts
@@ -0,0 +1,4 @@
+export { default as FullScreen } from "./FullScreen";
+export { default as Home } from "./Home";
+export { default as InfoCircle } from "./InfoCircle";
+export { default as User } from "./User";
